818a85286011190722e270707366d2e4dedee1b8
T Luchko <tluchko@csun.edu>
2020-06-11 10:24:53 -0700

    removing unused files

--- start.py
+++
@@ -1,9 +0,0 @@
-try:
-  import google.colab
-  get_ipython().system('pip install ipympl')
-  import importlib  
-  magneticrhr = importlib.import_module("right-hand-rules.magneticrhr")
-except:
-  import magneticrhr
-
-m = magneticrhr.MagneticRHR()
