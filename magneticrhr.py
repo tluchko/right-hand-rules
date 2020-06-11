1e78ff08efe2e6ddf5a5952e331009d59928ca6e
T Luchko <tluchko@csun.edu>
2020-06-10 18:57:53 -0700

    added some documentation for the MagneticRHR class

--- magneticrhr.py
+++ magneticrhr.py
@@ -9,7 +9,17 @@ get_ipython().run_line_magic('matplotlib', 'widget')
 #%matplotlib widget
 
 class MagneticRHR:
+    '''A class for students to test their knowledge of the magnetic force
+    right-hand-rule in Jupyter notebooks.  Calling the class will
+    start the self-test, which will run until the notebook is quit.
+
+    Only the simplest cases are currently handled. I.e., magnetic
+    fields and particle velocities are along the x-, y-, or z-axes.
+
+    '''
     def __init__(self):
+        '''Initialize the test.  It is automatically displayed.
+        '''
         self.directions = collections.OrderedDict(
             {( 0, 0, 0):'None',
              ( 1, 0, 0):'Right', 
@@ -18,64 +28,91 @@ class MagneticRHR:
              ( 0,-1, 0):'Down',
              ( 0, 0, 1):'Out of Page',
              ( 0, 0, -1):'Into Page'})
-        
+
+        # Dropdown menu to select the values from. Added a dummy answer as the starting point.
         self.direction_widget = widgets.Dropdown(
             options=['']+list(self.directions.values()),
             value=None,
             description='Direction:',
             disabled=False)
+        # callback for when a new value is selected.
         self.direction_widget.observe(self._guess, 'value')
-        
+
+        # display of the magnetif field plot
         self.display_widget = widgets.Output(layout = ipywidgets.Layout(width='50%'))
         with self.display_widget:
             self.fig, self.ax = plt.subplots(constrained_layout=True, figsize=(5, 5))
             self.ax.set_aspect('equal')
 
+        # output widget to tell the student if they are right or wrong
         self.output_widget = widgets.Output()
-        
+
+        # get the next example
         self.next_widget = widgets.Button(
             description='Next',
             disabled=False,
             button_style='', # 'success', 'info', 'warning', 'danger' or ''
             tooltip='Get next problem',
-            icon='check' )
+            #icon='arrow-right'
+        )
+        # callback function
         self.next_widget.on_click(self.next)
 
+        # stack the widgets and display the first problem
         display(widgets.VBox([self.display_widget, self.direction_widget, self.output_widget, self.next_widget]))
         self.display_problem()
         
-    def next(self, button):    
+    def next(self, button):
+        '''
+        Generate the next problem
+        '''
         self.display_problem()
         
     def display_problem(self):
+        '''
+        Generates and displays the next problem
+        '''
+
+        # generate a random B-field, particle velocity and charge from
+        # the fixed options.  The first direction is 'None' so ignore
+        # it.
         B_field = np.array(random.choice(list(self.directions.keys())[1:]))
         velocity = np.array(random.choice(list(self.directions.keys())[1:]))
         charge = random.choice([-1,1])
+        # calculate the answer
         force = tuple(charge*np.cross(velocity, B_field))
         self.correct = self.directions[force]
-        self.direction_widget.value=None
-        
+
+        # clear the previous response to the student
         self.output_widget.clear_output()
         with self.output_widget:
             print('\n')
 
+
+        # clear and recreate the plot.  `wait=True` is required to avoid flickering.
         self.display_widget.clear_output(wait=True)
-        
         self.ax.clear()
         self.ax.axis('off')
-
-            
         self.ax.set_xlim(-1, 1)
         self.ax.set_ylim(-1, 1)
-
         self._draw_B_field(B_field)
         self._draw_particle(charge, velocity)
 
+        # display the plot
         with self.display_widget:
             # print(B_field, velocity, charge, force)
             display(self.ax.figure)
 
     def _draw_particle(self, charge, velocity):
+        '''
+        Draws the particle velocity vector and label
+        
+        Args:
+            charge:
+                (-1 or 1)
+            velocity:
+                (list of ints) velocity vector
+        '''
         color='C0'
         if charge >0:
             particle_label = 'Positive particle'
@@ -83,6 +120,8 @@ class MagneticRHR:
             particle_label = 'Negative particle'
 
 
+        # into and out of the page require special treatment since we
+        # use special vector symbols in these cases
         if np.sum(np.abs(velocity[:2])) > 0:
             self.ax.quiver(
                 *list(-velocity[:2]/2), 
@@ -105,9 +144,21 @@ class MagneticRHR:
             self.ax.text(0,.15, particle_label, c=color, ha='center', va='top')
 
     def _draw_B_field(self, direction):
+        '''
+        Draws the B-field vector
+        
+        Args:
+            direction:
+                (list of ints) direction vector
+        '''
         color='C1'
         label = 'Magnetic field'
         nvectors = 11
+
+        # into and out of the page require special treatment since we
+        # use special vector symbols in these cases. Also, up/down and
+        # left/right need special treatment
+
         if direction[0] != 0:
             self.ax.quiver(
                 np.full([nvectors], -direction[0]), 
@@ -142,6 +193,9 @@ class MagneticRHR:
 
 
     def _guess(self, change):
+        '''Update the student and let them know if they are right or not.
+
+        '''
         self.output_widget.clear_output()
         with self.output_widget:
             if change['new']==self.correct:
